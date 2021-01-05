a4d23f4a538d03249304253101754ec70dab9c7e
Manuel <jmoralesd@ia.com.mx>
2021-01-05 11:19:09 -0600

    Connect to mongo database
    Mongoose package installed to manage the database, mongoDb file created
    to store the connection

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./network/routes');
+const mongoDB = require('./mongoDB');
 
+mongoDB('mongodb://localhost:27017/contapp');
 const app = express();
 router(app);
 app.use(bodyParser.json());
