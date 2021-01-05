17a14c80c50ad92fb90a8c8f20346ea361376d0b
Manuel <jmoralesd@ia.com.mx>
2021-01-05 14:30:38 -0600

    fix: Call body-parser before use router.
    If you don't use body-parser before using the router, the library
    doesn't work on the router added before.

--- src/backend/server.js
+++ src/backend/server.js
@@ -5,8 +5,8 @@ const mongoDB = require('./mongoDB');
 
 mongoDB('mongodb://localhost:27017/contapp');
 const app = express();
-router(app);
 app.use(bodyParser.json());
+router(app);
 app.listen(3000, () => {
   console.log('server on');
 });
