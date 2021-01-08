b41c6c255441b361c176c2b53763b83391f4e0f8
Manuel <jmoralesd@ia.com.mx>
2021-01-07 18:48:14 -0600

    Feature: Validate account form.
    react-hook-form was used to validate form fields.

--- src/frontend/components/AccountForm.jsx
+++ src/frontend/components/AccountForm.jsx
@@ -1,45 +1,66 @@
 import React from 'react';
 import '../assets/styles/components/AccountForm.scss';
+import { useForm } from 'react-hook-form';
 
 const AccountForm = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  const firstNumberIdentifier = (value) => {
+    const firstNumber = String(value).charAt(0);
+    if (firstNumber > 0 && firstNumber <= 5) {
+      return true;
+    }
+    return false;
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <h1>Crea un nueva cuenta</h1>
       <label htmlFor='identifier'>
         Identificador
-        <input id='identifier' placeholder='Agrega un identificador numerico' type='text' maxLength='8' />
+        <input ref={register({ required: 'Este campo es requirido', valueAsNumber: true, validate: (value) => firstNumberIdentifier(value) || 'el primer numero debe ser entre 1 y 5', maxLength: { value: 8, message: 'El identificador no debe ser mayor a 8 digitos' } })} name='identifier' placeholder='Agrega un identificador numerico' type='number' />
+        {errors.identifier && <p>{ errors.identifier.message }</p>}
       </label>
       <label htmlFor='description'>
         Descripcion
-        <textarea id='description' placeholder='Agrega una descripcion' rows='5' />
+        <textarea ref={register({ required: 'Este campo es requirido' })} name='description' placeholder='Agrega una descripcion' rows='5' />
+        {errors.description && <p>{ errors.description.message }</p>}
       </label>
       <label htmlFor='level'>
         Nivel
-        <select id='level'>
+        <select ref={register({ required: 'Este campo es requirido', valueAsNumber: true })} name='level'>
+          <option value=''>...</option>
           <option>1</option>
           <option>2</option>
           <option>3</option>
           <option>4</option>
           <option>5</option>
         </select>
+        {errors.level && <p>{ errors.level.message }</p>}
       </label>
       <label htmlFor='type'>
         Tipo
-        <select id='type'>
+        <select ref={register({ required: 'Este campo es requirido', valueAsNumber: true })} name='type'>
+          <option value=''>...</option>
           <option>1</option>
           <option>2</option>
         </select>
+        {errors.type && <p>{ errors.type.message }</p>}
       </label>
       <label htmlFor='keycontrol'>
         Llave de control
-        <input id='keycontrol' placeholder='Agrega una llave de control numerica valida' type='text' maxLength='8' />
+        <input ref={register({ required: 'Este campo es requirido', valueAsNumber: true, validate: (value) => firstNumberIdentifier(value) || 'el primer numero debe ser entre 1 y 5', maxLength: { value: 8, message: 'La llave no debe ser mayor a 8 digitos' } })} name='keycontrol' placeholder='Agrega una llave de control numerica valida' type='number' />
+        {errors.keycontrol && <p>{ errors.keycontrol.message }</p>}
       </label>
       <label htmlFor='balance'>
         Tipo de saldo
-        <select id='balance'>
+        <select ref={register({ required: 'Este campo es requirido' })} name='balance'>
+          <option value=''>...</option>
           <option>Deudor</option>
           <option>Acreedor</option>
         </select>
+        {errors.balance && <p>{ errors.balance.message }</p>}
       </label>
       <button type='submit'>Crear</button>
     </form>
