edb8b3e0c47428b61fdfd699bf272260093ae6b3
Manuel <jmoralesd@ia.com.mx>
2021-01-19 10:58:02 -0600

    Feature: Add Navbar to layout.
    We remove the styles and link from AccountTable component because Navbar
    component will manages this elementes.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
 
@@ -9,6 +10,7 @@ const Layout = ({ children }) => {
   }, []);
   return (
     <>
+      <Navbar />
       {children}
     </>
   );
