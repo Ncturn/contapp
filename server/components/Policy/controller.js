03bd6d70bac4a29000a4c76873e3761aff5838a3
Manuel <jmoralesd@ia.com.mx>
2021-01-22 15:42:34 -0600

    Feature: Validate policies request.
    The folder utils was maded to store custom methods that will be used
    in several files, policy controller was created to validate fet and post
    requests.

---
+++ server/components/Policy/controller.js
@@ -0,0 +1,41 @@
+const store = require('./store');
+const Validator = require('../../utils/CustomValidator');
+const errorResponse = require('../../utils/ErrorResponse');
+
+const getPolicy = async (policyFilter) => {
+  const filter = {};
+  if (policyFilter) {
+    filter.identifier = policyFilter;
+  }
+  const policies = await store.find(filter);
+  return {
+    code: 200,
+    body: policies,
+    error: null,
+  };
+};
+
+const createPolicy = async (policy) => {
+  const policyValidator = new Validator({
+    ...policy,
+  }, {
+    date: 'required|date',
+    identifier: 'required|string|length:5,5|policy',
+    consecutive: 'required|integer|digits:3',
+    account: 'required|string|length:8,1|account:5',
+    concept: 'required|string',
+    amount: 'required|decimal',
+    type: 'required|string|accepted:cargo,abono',
+  });
+  const matched = await policyValidator.check();
+  if (matched) {
+    return store.save(policy);
+  }
+  const response = errorResponse(policyValidator.errors);
+  return response;
+};
+
+module.exports = {
+  getPolicy,
+  createPolicy,
+};
