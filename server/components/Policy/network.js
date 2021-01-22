52936dbe8b970286fd08434ca69537ec5beaef8d
Manuel <jmoralesd@ia.com.mx>
2021-01-22 15:44:52 -0600

    Feature: Create Policy router.
    The Policy ruoter was created for manage GET and POST request, and was
    added to routes files to be added to express server.

---
+++ server/components/Policy/network.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const controller = require('./controller');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const filter = req.query.policy || null;
+  const response = await controller.getPolicy(filter);
+  res.status(response.code).send({
+    error: response.error,
+    body: response.body,
+  });
+});
+
+router.post('/', async (req, res) => {
+  const response = await controller.createPolicy(req.body);
+  res.status(response.code).send({
+    error: response.error,
+    body: response.body,
+  });
+});
+
+module.exports = router;
