11c075bf1118c3bceac3215e88183b79ed6bcf69
Manuel <jmoralesd@ia.com.mx>
2021-01-28 17:35:12 -0600

    Use Account component instead AccountTable.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,7 +4,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPencilAlt, faTrashAlt, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
 import editAccount from '../views/editAccount';
 import createAccount from '../views/createAccount';
-import AccountTable from '../components/AccountTable';
+import Account from '../components/Account';
 import Policy from '../components/Policy';
 import CreatePolicy from '../views/CreatePolicy';
 import EditPolicy from '../views/EditPolicy';
@@ -16,8 +16,8 @@ const App = () => {
     <HashRouter>
       <Layout>
         <Switch>
-          <Route exact path='/' component={AccountTable} />
-          <Route exact path='/account' component={AccountTable} />
+          <Route exact path='/' component={Account} />
+          <Route exact path='/account' component={Account} />
           <Route exact path='/account/create' component={createAccount} />
           <Route exact path='/account/edit/:identifier' component={editAccount} />
           <Route exact path='/policy' component={Policy} />
