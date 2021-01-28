2522ac03727516347ebaf5ddfd4536ce89a81f4e
Manuel <jmoralesd@ia.com.mx>
2021-01-28 15:17:39 -0600

    Refactor: Navigation and add plus icon

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPencilAlt, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faPencilAlt, faTrashAlt, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
 import editAccount from '../views/editAccount';
 import createAccount from '../views/createAccount';
 import AccountTable from '../components/AccountTable';
@@ -11,14 +11,15 @@ import EditPolicy from '../views/EditPolicy';
 import Layout from '../components/Layout';
 
 const App = () => {
-  library.add(faPencilAlt, faTrashAlt);
+  library.add(faPencilAlt, faTrashAlt, faPlusSquare);
   return (
     <HashRouter>
       <Layout>
         <Switch>
           <Route exact path='/' component={AccountTable} />
-          <Route exact path='/create' component={createAccount} />
-          <Route exact path='/edit/:identifier' component={editAccount} />
+          <Route exact path='/account' component={AccountTable} />
+          <Route exact path='/account/create' component={createAccount} />
+          <Route exact path='/account/edit/:identifier' component={editAccount} />
           <Route exact path='/policy' component={Policy} />
           <Route exact path='/policy/create' component={CreatePolicy} />
           <Route exact path='/policy/edit/:identifier' component={EditPolicy} />
