28312bedc387d47000340eeb839deca9ee909683
Manuel <jmoralesd@ia.com.mx>
2021-01-28 15:11:58 -0600

    Refactor: Create a button to create a new item.
    Now in the main view for each collection will exist an icon to create,
    delete and edit a item, styles for the nes button was added and the
    identifier was change for a tag html instead account id.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import TableRow from './TableRow';
 import '../assets/styles/components/AccountTable.scss';
 
-const Table = ({ title, items, fields, handleTrashClick, handlePencilClick }) => {
+const Table = ({ title, items, fields, handleTrashClick, handlePencilClick, handlePlusClick }) => {
   return (
     <div>
-      <h1 id='title'>{title}</h1>
-      <table id='account'>
+      <div className='table-title'>
+        <h1>{title}</h1>
+        <FontAwesomeIcon onClick={handlePlusClick} className='plus-icon' icon='plus-square' />
+      </div>
+      <table>
         <tbody>
           <tr>
             {
