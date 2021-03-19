9cfe69cdac45dec40e4a6d18d2b96768743b4491
Manuel <jmoralesd@ia.com.mx>
2021-03-19 13:17:08 -0600

    Feature: Create input search for table.
    Is a user add a valid identifier the table will show it, when the input
    is empty the table will show all items.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import TableRow from './TableRow';
+import { handleFilterChange } from '../utils/TableEvents';
 import '../assets/styles/components/AccountTable.scss';
 
-const Table = ({ title, items, fields, handleTrashClick, handlePencilClick, handlePlusClick }) => {
+const Table = ({ title, items, fields, getCollection, handleTrashClick, handlePencilClick, handlePlusClick }) => {
   return (
     <div>
       <div className='table-title'>
+        <input className='filterInput' type='text' onChange={(event) => { handleFilterChange(event, getCollection); }} name='filter' placeholder='identificador' />
         <h1>{title}</h1>
         <FontAwesomeIcon onClick={handlePlusClick} className='plus-icon' icon='plus-square' />
       </div>
