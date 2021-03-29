16075870d08efbdf51670736ca56e41a42fb4193
Manuel <jmoralesd@ia.com.mx>
2021-03-28 18:58:26 -0600

    Feature: Add request to post and patch methods

--- src/utils/CollectionApi.js
+++ src/utils/CollectionApi.js
@@ -20,8 +20,20 @@ const deleteItem = async (collection, identifier) => {
   const data = await response.json();
   return data;
 };
+const createItem = async (collection, httpMethod, item) => {
+  const response = await fetch(`http://localhost:3000/${collection}`, {
+    method: httpMethod,
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(item),
+  });
+  const data = await response.json();
+  return data;
+};
 
 export {
   getCollection,
   deleteItem,
+  createItem,
 };
